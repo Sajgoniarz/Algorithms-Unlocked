914ee81ff91c182bf3de115706fb2ea1a2dca6cc
Marek <Sajgoniarz1@gmail.com>
2016-05-29 10:08:57 +0200

    Add: Selection Sort

---
+++ 1. Sorting And Searching/Selection Sort/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void showArray(int[] a)
+        {
+            for (int k = 0; k < a.Length; k++)
+            {
+                Console.Write("{0},", a[k]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Selection Sort");
+            Console.WriteLine("Type - size");
+            int size = Int32.Parse(Console.ReadLine());
+            int[] a = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = r.Next(size * 5);
+            }
+            Console.Write("Before: "); showArray(a);
+            Console.Write("\nAfter: "); showArray(SelectionSort.Sort(a));
+            Console.Write("\n");
+        }
+    }
+}
---
+++ 1. Sorting And Searching/Selection Sort/SelectionSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Selection_Sort
+{
+    static class SelectionSort
+    {
+        public static int[] Sort(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                int min = i;
+                int temp = 0;
+                for (int j = i + 1; j < a.Length; j++)
+                {
+                    if (a[min] > a[j])
+                        min = j;
+                }
+
+                temp = a[min];
+                a[min] = a[i];
+                a[i] = temp;
+            }
+            return a;
+        }
+    }
+}
