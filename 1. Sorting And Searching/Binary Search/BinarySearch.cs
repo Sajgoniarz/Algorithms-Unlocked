8a45496428742fe3ac535129e29f1506d22091ee
Marek <Sajgoniarz1@gmail.com>
2016-05-29 09:53:11 +0200

    Add: Binary Search

---
+++ 1. Sorting And Searching/Binary Search/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    class BinarySearch
+    {
+        public static int Search(int[] a, int x)
+        {
+            int p = 0;
+            int r = a.Length - 1;
+            int m = 0;
+            int i = 0;
+            while (p <= r)
+            {
+                i++;
+
+                m = p + ((r - p) / 2);
+
+                if (a[m] > x)
+                    r = m - 1;
+                else if (a[m] < x)
+                    p = m + 1;
+                else
+                    return m;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 1. Sorting And Searching/Binary Search/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_Search
+{
+    class Program
+    {
+        static int[] GenerateArray(int size)
+        {
+            int[] temp = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                temp[i] = i;
+            }
+            return temp;
+        }
+        static void Main(string[] args)
+        {
+            int key, size, result;
+            string parameters;
+            Console.WriteLine("Binary Search");
+            Console.WriteLine("Type - size:key");
+            parameters = Console.ReadLine();
+
+            size = Int32.Parse(parameters.Split(':')[0]);
+            key = Int32.Parse(parameters.Split(':')[1]);
+
+            result = BinarySearch.Search(GenerateArray(size), key);
+
+            if (result != -1)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("{1} found at {0} position.", result, key);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("{0} not found.", key);
+            }
+        }
+    }
+};
