c613ec1c65af375d0ae199d8acdb426d7e34ccc0
Marek <Sajgoniarz1@gmail.com>
2016-05-29 10:04:59 +0200

    Add: Recursive Binary Search

---
+++ 1. Sorting And Searching/Recursive Binary Search/RecursiveBinarySearch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Recursive_Binary_Search
+{
+    static class RecursiveBinarySearch
+    {
+        public static int Search(int[] a, int p, int r, int x)
+        {
+            if (p > r)
+                return -1;
+
+            int m = p + ((r - p) / 2);
+
+            if (a[m] > x)
+                return Search(a, p, m - 1, x);
+            else if (a[m] < x)
+                return Search(a, m + 1, r, x);
+            else
+                return m;
+        }
+
+        public static int Search(int[] a, int x)
+        {
+            return Search(a, 0, a.Length - 1, x);
+        }
+
+    }
+}
