7a34d70798307c9f25f0804692830ff809074ab4
Marek <Sajgoniarz1@gmail.com>
2016-05-31 09:23:58 +0200

    New: Lower Bound for Sorting Add: Really Simple Sort

---
+++ 2. A Lower Bound for Sorting/Really Simple Sort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Really_Simple_Sort
+{
+    class Program
+    {
+        static void showArray(int[] a)
+        {
+            for (int k = 0; k < a.Length; k++)
+            {
+                Console.Write("{0},", a[k]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Really Simple Sort");
+            Console.WriteLine("Type - size");
+            int size = Int32.Parse(Console.ReadLine());
+            int[] a = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = r.Next(1,3);
+            }
+
+            Console.Write("Before: "); showArray(a);
+            Console.Write("\nAfter: "); showArray(ReallySimpleSort.Sort(a, a.Length));
+            Console.Write("\n");
+        }
+    }
+}
---
+++ 2. A Lower Bound for Sorting/Really Simple Sort/ReallySimpleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Really_Simple_Sort
+{
+    static class ReallySimpleSort
+    {
+        public static int[] Sort(int[] a, int n)
+        {
+            int k = 0;
+            int i = 0;
+
+            for (i = 0; i < n; i++)
+                if (a[i] == 1)
+                    k++;
+
+            for (i = 0; i < k; i++)
+                a[i] = 1;
+
+            for (i = k; i < n; i++)
+                a[i] = 2;
+
+            return a;
+        }
+    }
+}
