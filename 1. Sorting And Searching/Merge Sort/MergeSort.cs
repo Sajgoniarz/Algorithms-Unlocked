1a69410035b300908212794c18b2db3a1f61754a
Marek <Sajgoniarz1@gmail.com>
2016-05-31 07:52:14 +0200

    Add: Merge Sort

---
+++ 1. Sorting And Searching/Merge Sort/MergeSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Sort
+{
+    static class MergeSort
+    {
+        private static int[] Merge(int[] a, int p, int q, int r)
+        {
+            int n1 = q - p + 1;
+            int n2 = r - q;
+
+            int[] b = new int[n1];
+            int[] c = new int[n2];
+
+            Array.Copy(a, p, b, 0, n1);
+            Array.Copy(a, q+1, c, 0, n2);
+
+            int i = 0;
+            int j = 0;
+            for (int k = p; k <= r; k++)
+            {
+                if(i == b.Length)
+                {
+                    a[k] = c[j];
+                    j++;
+                }
+                else if (j == c.Length)
+                {
+                    a[k] = b[i];
+                    i++;
+                } else if (b[i] <= c[j])
+                {
+                    a[k] = b[i];
+                    i++;
+                } else
+                {
+                    a[k] = c[j];
+                    j++;
+                }
+            }
+            return a;
+        }
+
+        public static int[] Sort(int[] a, int p, int r)
+        {
+            if (p >= r) return a;
+
+            int q = (p + r) / 2;
+            Sort(a, p, q);
+            Sort(a, q + 1, r);
+            return Merge(a, p, q, r);
+        }
+    }
+}
---
+++ 1. Sorting And Searching/Merge Sort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Merge_Sort
+{
+    class Program
+    {
+        static void showArray(int[] a)
+        {
+            for (int k = 0; k < a.Length; k++)
+            {
+                Console.Write("{0},", a[k]);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Merge Sort");
+            Console.WriteLine("Type - size");
+            int size = Int32.Parse(Console.ReadLine());
+            int[] a = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                a[i] = r.Next(size * 5);
+            }
+            Console.Write("Before: "); showArray(a);
+            Console.Write("\nAfter: "); showArray(MergeSort.Sort(a, 0, a.Length - 1));
+            Console.Write("\n");
+        }
+    }
+}
