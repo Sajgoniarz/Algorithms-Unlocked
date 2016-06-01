51b307251294db36a0824923728b1ffdf1c2d2c6
Marek <Sajgoniarz1@gmail.com>
2016-06-01 09:19:18 +0200

    Add: Counting Sort

---
+++ 2. A Lower Bound for Sorting/Counting Sort/CountingSort.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_Sort
+{
+    static class CountingSort
+    {
+        public static int[] Sort(int[]a, int n, int m)
+        {
+            int[] equal = CountEqual(a, n, m);
+            int[] less = CountLess(equal, m);
+            return Rearrange(a, less, n, m);
+        }
+
+        private static int[] CountEqual(int[] a, int n, int m)
+        {
+            int[] equal = new int[m];
+            int i;
+
+            for (i = 0; i < m; i++)
+                equal[i] = 0;
+
+            for (i = 0; i < n; i++)
+                equal[a[i]]++;
+
+            return equal;
+        }
+
+        private static int[] CountLess(int[] a, int m)
+        {
+            int[] temp = new int[m];
+            temp[0] = 0;
+
+            for (int i = 1; i < m; i++)
+                temp[i] = temp[i - 1] + a[i - 1];
+
+            return temp;
+        }
+
+        private static int[] Rearrange(int[] a, int[] less, int n, int m)
+        {
+            int[] b = new int[n];
+            int[] next = new int[m];
+
+            for (int i = 0; i < m; i++)
+                next[i] = less[i];
+
+            for (int i = 0; i < n; i++)
+            {
+                int key = a[i];
+                int index = next[key];
+
+                b[index] = a[i];
+                next[key]++;
+            }
+
+            return b;
+        }
+    }
+}
---
+++ 2. A Lower Bound for Sorting/Counting Sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Counting_Sort
+{
+    class Program
+    {
+        static int[] BuildArray(int n, int m)
+        {
+            Random r = new Random();
+            int[] temp = new int[n];
+
+            for (int i = 0; i < n; i++)
+                temp[i] = r.Next(0, m);
+
+            return temp;
+        }
+
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Counting Sort");
+                Console.WriteLine("n - size of array, m - number of diffrent keys");
+                Console.WriteLine("Type n:m");
+                string input = Console.ReadLine();
+
+                int n = Int32.Parse(input.Split(':')[0]);
+                int m = Int32.Parse(input.Split(':')[1]);
+
+                int[] tab = BuildArray(n, m);
+                int[] sorted = CountingSort.Sort(tab, n, m);
+
+                Console.Write("Before :");
+                for (int i = 0; i < n; i++)
+                    Console.Write("{0},", tab[i]);
+                Console.Write("\nAfter : ");
+                for (int i = 0; i < n; i++)
+                    Console.Write("{0},", sorted[i]);
+                Console.WriteLine();
+         
+        }
+    }
+}
